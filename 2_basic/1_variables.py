e0887b284644fb8617b7eb15e67019d7465aa87a
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-18 10:22:17 +0200

    1

---
+++ 2_basic/1_variables.py
@@ -0,0 +1,14 @@
+print("Hello world")
+print('Worst Wednesday Ever')
+
+a = 5            #int
+b = 6.5          #float
+c = 'Wednesday'  #str
+
+print('a')  #printed letter a
+print(a)    #printed variable a
+print(c)
+
+print(a+b)
+print(a * 3 * b - 3)
+print(a + c)
