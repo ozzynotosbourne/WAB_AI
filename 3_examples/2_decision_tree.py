ed0b0bae48d99ec6bdf79e9d92e464adfccf402b
musiakam <kamil.musial@tietoevry.com>
2023-11-08 10:30:28 +0100

    1

--- 3_examples/2_decision_tree.py
+++ 3_examples/2_decision_tree.py
@@ -1,3 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv("iris.csv")
+print(df)
+
+species = {
+    "Iris-setosa":0, "Iris-versicolor":1, "Iris-virginica":2
+}
+df["class_value"] = df["class"].map(species)
+print(df)
+X = df.iloc[:, :4]  # take 4 first columns
+y = df.class_value
+model = DecisionTreeClassifier()
+model.fit(X, y)
