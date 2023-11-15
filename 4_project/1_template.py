848e5994affc7a205a7f0a7a6df2db984be685a4
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-15 08:57:16 +0100

    1

--- 4_project/1_template.py
+++ 4_project/1_template.py
@@ -1,6 +1,18 @@
 from sklearn.datasets import make_circles
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
 
-X, y = make_circles(n_samples=100, factor=0.2, noise=0.3)
+X, y = make_circles(n_samples=50, factor=0.2, noise=0.4)
 plt.scatter(x=X[:, 0], y=X[:, 1], c=y)
 plt.show()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+from sklearn.linear_model import LogisticRegression
+print('\nLogistic regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
