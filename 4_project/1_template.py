6cd4d69522502986b64c3fc8305dcf4664591a70
ozzynotosbourne <151469020+ozzynotosbourne@users.noreply.github.com>
2023-11-19 21:29:20 +0100

    Osman

--- 4_project/1_template.py
+++ 4_project/1_template.py
@@ -2,45 +2,48 @@ from sklearn.datasets import make_circles
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 
+# Generate data using make_circles
 X, y = make_circles(n_samples=1000, factor=0.2, noise=0.4)
 plt.scatter(x=X[:, 0], y=X[:, 1], c=y)
 plt.show()
 
-from sklearn.model_selection import train_test_split
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-from sklearn.linear_model import LogisticRegression
-print('\nLogistic regression')
-model = LogisticRegression(solver='liblinear', max_iter=2000, multi_class='ovr', verbose=0)   #choose the model
-model.fit(X_train, y_train)    #teach the model
-print(model.score(X_test, y_test))      #print accuracy
-print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))    #print details
+# Logistic Regression
+print('\nLogistic Regression')
+model_lr = LogisticRegression(solver='liblinear', max_iter=5000, multi_class='ovr', penalty='l2')
+model_lr.fit(X_train, y_train)
+print('Accuracy:', model_lr.score(X_test, y_test))
+print('Confusion Matrix:')
+print(pd.DataFrame(confusion_matrix(y_test, model_lr.predict(X_test))))
 
-from sklearn.neighbors import KNeighborsClassifier
+# K-Nearest Neighbors (KNN)
 print('\nKNN')
-model = KNeighborsClassifier(n_neighbors=530, weights='distance', n_jobs=10)
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
-print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+model_knn = KNeighborsClassifier(n_neighbors=10, weights='distance', n_jobs=-1, p=1)
+model_knn.fit(X_train, y_train)
+print('Accuracy:', model_knn.score(X_test, y_test))
+print('Confusion Matrix:')
+print(pd.DataFrame(confusion_matrix(y_test, model_knn.predict(X_test))))
 
-from sklearn.tree import DecisionTreeClassifier
-print('\nDecission  Tree Clasifier')
-model = DecisionTreeClassifier(criterion='entropy', max_depth=5, min_samples_split=5)
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
-print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+# Decision Tree Classifier
+print('\nDecision Tree Classifier')
+model_dt = DecisionTreeClassifier(criterion='entropy', max_depth=8, min_samples_split=10, min_samples_leaf=5)
+model_dt.fit(X_train, y_train)
+print('Accuracy:', model_dt.score(X_test, y_test))
+print('Confusion Matrix:')
+print(pd.DataFrame(confusion_matrix(y_test, model_dt.predict(X_test))))
 
-from sklearn.svm import SVC
+# Support Vector Machine (SVC)
 print('\nSVC')
-model = SVC(kernel='poly', degree=10, gamma='auto', decision_function_shape='ovo')
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
-print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
-
-# ................. project
-# choose "make_circles" parameters and generate your data
-# find possibly best solution using at least 2 algorithms
-# investigate parameters mentioned in example
-# add at least one more parameter to each algorithm (not verbose, not random state)
-# deliver code + pdf report on moodle till day of our last class
+model_svc = SVC(kernel='rbf', degree=3, gamma='scale', decision_function_shape='ovo', C=0.5)
+model_svc.fit(X_train, y_train)
+print('Accuracy:', model_svc.score(X_test, y_test))
+print('Confusion Matrix:')
+print(pd.DataFrame(confusion_matrix(y_test, model_svc.predict(X_test))))
